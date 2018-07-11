500c469dba9bc3621478822b27c64cdb1d03672c
wasifmiraz <wasifmiraz@mail.com>
2018-07-11 18:42:54 +0600

    time complexity update

---
+++ time complex/exampleone.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+
+int main() {
+   
+ int n1, n2, result; //big ohh notation  0(1) this program operation constant that why big ohh 0(1)
+ 
+ n1 = 10;
+   n2 = 25;
+   
+ result=n1+n2;
+  
+return 0;
+ 
+}
