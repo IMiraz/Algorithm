b05b28fb230f8064726c1df246af03c8ee78570f
wasifmiraz <wasifmiraz@mail.com>
2018-07-17 16:43:41 +0600

    linear search update

---
+++ linear search/linear_search ex.c
@@ -0,0 +1,14 @@
+
+    int linear_search(int A[], int n, init x)
+{
+    int i;
+
+     for(i = 0; i<n; i++) {
+          if(A[i] == x) {
+               return i ;
+          }
+     }
+     i = -1;
+ return i ;
+
+ }
